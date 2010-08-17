8bfc4dd131672bf8bdd4082493a2b17116a9fbe9
Dopi <dopi711@googlemail.com>
2010-08-17 18:29:50 +0200

    added keypad_fix.sh

---
+++ keypad_fix.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+#check if we are in the right working directory
+if [ -f make_buildpack.sh ]; then 
+cd ..
+fi 
+
+chmod 644 source/out/target/product/GT-S8000/system/usr/keychars/*
+echo "key 11    VOLUME_UP" >source/out/target/product/GT-S8000/system/usr/keylayout/qwerty.kl
+echo "key 10    VOLUME_DOWN" >>source/out/target/product/GT-S8000/system/usr/keylayout/qwerty.kl
+echo "key 9     CALL              WAKE_DROPPED" >>source/out/target/product/GT-S8000/system/usr/keylayout/qwerty.kl
+echo "key 1     MENU              WAKE" >>source/out/target/product/GT-S8000/system/usr/keylayout/qwerty.kl
+echo "key 249   ENDCALL           WAKE_DROPPED" >>source/out/target/product/GT-S8000/system/usr/keylayout/qwerty.kl
+echo "key 2     CAMERA" >>source/out/target/product/GT-S8000/system/usr/keylayout/qwerty.kl
+echo "key 3     FOCUS" >>source/out/target/product/GT-S8000/system/usr/keylayout/qwerty.kl
+echo "key 19    HOME              WAKE_DROPPED" >>source/out/target/product/GT-S8000/system/usr/keylayout/qwerty.kl
+echo "key 17    BACK              WAKE_DROPPED" >>source/out/target/product/GT-S8000/system/usr/keylayout/qwerty.kl
+
