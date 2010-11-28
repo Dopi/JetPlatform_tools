0318423d9243eff47c0ebfe097179319db686e31
Dopi <dopi711@googlemail.com>
2010-11-29 00:42:23 +0100

    added more custom repos for update

--- update_mirrors.sh
+++ update_mirrors.sh
@@ -13,8 +13,9 @@ git fetch origin
 git branch eclair
 git checkout origin/eclair
 git pull origin eclair
-git push githiub --all
+git push github --all
 cd ../../../../
+
 cd source/packages/apps/Openmanager/
 git remote rm origin
 git remote add origin git://github.com/nexes/Android-File-Manager.git
@@ -23,8 +24,9 @@ git remote add github git@github.com:Dopi/JetPlatform_packages_apps_openmanager.
 git fetch origin
 git checkout master
 git pull origin master
-git push githiub --all
+git push github --all
 cd ../../../../
+
 cd source/packages/apps/Superuser/
 git remote rm origin
 git remote add origin git://github.com/ChainsDD/android_packages_apps_Superuser.git
@@ -33,8 +35,31 @@ git remote add github git@github.com:Dopi/JetPlatform_packages_apps_superuser.gi
 git fetch origin
 git checkout master
 git pull origin master
-git push githiub --all
+git push github --all
 cd ../../../../
+
+cd source/external/dhcpcd
+git remote rm origin
+git remote add origin git://android.kernel.org/platform/external/dhcpcd.git
+git remote rm github
+git remote add github git@github.com:Dopi/JetPlatform_external_dhcpcd.git
+git fetch origin
+git checkout eclair
+git pull origin eclair
+git push github --all
+cd ../../../
+
+cd source/hardware/libhardware_legacy
+git remote rm origin
+git remote add origin git://android.git.kernel.org/platform/hardware/libhardware_legacy.git
+git remote rm github
+git remote add github git@github.com:Dopi/JetPlatform_hardware_libhardware_legacy.git
+git fetch origin
+git checkout eclair
+git pull origin eclair
+git push github --all
+cd ../../../
+
 cd source/build/
 git remote rm origin
 git remote add origin git://android.kernel.org/platform/build.git
